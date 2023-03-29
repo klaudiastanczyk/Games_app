ab67a7adef31ae9161c11158b365ca4cc780ea5b
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2023-03-29 20:14:03 +0200

    Add new color / font / hover

--- src/components/Navbar/Navbar.styles.js
+++ src/components/Navbar/Navbar.styles.js
@@ -6,25 +6,34 @@ export const NavbarContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
-  background-color: red;
+  background-color: #2F4858;
+  
 `
 export const NavbarStyled = styled.div`
-  width:  60%;
-  margin: auto;
+  width:  80%;
+  margin: 10px auto 0;
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
+  
 `
 
 export const GameNameTile = styled.div`
+  margin-right: 1px;
   width: 20%;
-  background-color: ${({isActiveGame}) => isActiveGame ? 'blue' : 'yellow' };
+  background-color: ${({isActiveGame}) => isActiveGame ? '#EED374' : '#DDF0E2' };
+  color: ${({isActiveGame}) => isActiveGame ? '#2F4858' : '#046970' };
   cursor: pointer;
   align-items: center;
   display: flex;
   justify-content: center;
-
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  &:hover{
+    background-color: ${({isActiveGame}) => isActiveGame ? '#EED374' : '#D3FADE' };
+  }
 `
 export const Text = styled.h1`
   font-size: 20px;
+  font-family: 'Yantramanav', sans-serif;
 `
